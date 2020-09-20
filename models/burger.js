af5f1b6b46c1dcb78b6668a7b9b840d3815bc19a
Reed Santos <reedsantos@icloud.com>
2020-09-20 15:16:52 -0400

    add comments

--- models/burger.js
+++ models/burger.js
@@ -1,29 +1,31 @@
+// Import orm.js
 var orm = require("../config/orm.js");
-
+// Calling orm functions
 var burger = {
+    // Show all burgers
     selectAll: function(cb) {
         orm.selectAll("burgers", function(res) {
             cb(res);
         });
     },
-
+    // Add burger
     insertOne: function(cols, vals, cb) {
         orm.insertOne("burgers", cols, vals, function(res) {
             cb(res);
         });
     },
-
+    // Devour burger
     updateOne: function(objColVals, condition, cb) {
         orm.updateOne("burgers", objColVals, condition, function(res) {
             cb(res);
         });
     },
-
+    // Delete burger
     deleteOne: function(condition, cb) {
         orm.deleteOne("burgers", condition, function(res) {
             cb(res);
         });
     }
 };
-
+// Export
 module.exports = burger;
