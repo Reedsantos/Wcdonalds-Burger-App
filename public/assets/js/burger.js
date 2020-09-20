0783bb9942a7989f1039895ea00fb2c81a51aca6
Reed Santos <reedsantos@icloud.com>
2020-09-20 15:11:44 -0400

    add delete click handler

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -32,4 +32,17 @@ $(function() {
             location.reload();
         });
     });
+
+    $(".deleteBurger").on("click", function(event) {
+        event.preventDefault();
+
+        var id = $(this).data("id");
+
+        
+        $.ajax({
+            type: "DELETE",
+            url: "/api/burgers/" + id
+        }).then(location.reload());
+    });
+
 });
