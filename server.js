139c25585b917e76003a913d6f2abf1fe1e8264b
Reed Santos <reedsantos@icloud.com>
2020-09-20 16:11:47 -0400

    change port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
-var PORT = process.env.PORT || 3026;
+var PORT = process.env.PORT || 3027;
 
 // Serve content
 app.use(express.static("public"));
