765b338625d83b35b4a35d3789768e3906704830
Reed Santos <reedsantos@icloud.com>
2020-09-20 15:18:08 -0400

    add comments

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
+// Dependencies
 var express = require("express");
 var bodyParser = require("body-parser");
 
 var app = express();
 var PORT = process.env.PORT || 3025;
 
+// Serve content
 app.use(express.static("public"));
+
+// parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Handlebars
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// routes
 var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
