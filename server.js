9fd486e4e8ddf23706b6a3daa842ff66ee9776cf
Reed Santos <reedsantos@icloud.com>
2020-09-20 15:06:41 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 3025;
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
 });
